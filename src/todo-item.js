1a4340fbdd2542ba6cb049da569cec9fa0a429f3
Ted Falasco <14131511+tfalasco@users.noreply.github.com>
2025-03-12 05:07:26 -0400

    Add todo module

---
+++ src/todo-item.js
@@ -0,0 +1,10 @@
+export { todo };
+
+class todo {
+    constructor (title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
