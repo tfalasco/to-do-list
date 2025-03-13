8264f817adbc1ffbc190a451fb0e76717ee96e2f
Ted Falasco <14131511+tfalasco@users.noreply.github.com>
2025-03-13 05:31:32 -0400

    Add Priority type and getter/setters
    Also specify parameter types for constructor

--- src/todo-item.js
+++ src/todo-item.js
@@ -1,10 +1,45 @@
-export { Todo };
+import { Log } from "./logger.js";
+
+export { Todo, Priority };
+
+const Priority = Object.freeze ({
+    NONE: 0,
+    LOW: 1,
+    MEDIUM: 2,
+    HIGH: 3,
+});
 
 class Todo {
+    _priority
+
+    /**
+     * @param {String} title
+     * @param {String} description
+     * @param {Date} dueDate
+     * @param {Priority} priority
+     */
     constructor (title, description, dueDate, priority) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+    }
+
+    /**
+     * @param {Priority} newPriority
+     */
+    set priority(newPriority) {
+        if ((newPriority >= Priority.NONE) &&
+            (newPriority <= Priority.HIGH)) {
+                this._priority = newPriority;
+                Log.v(`Set new priority ${newPriority}`);
+        }
+        else {
+            Log.e("Could not set invalid priority.");
+        }
+    }
+
+    get priority() {
+        return this._priority;
     }
 }
