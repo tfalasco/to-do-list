f372ef2f0b3403d6433f6324609853a16aa345d1
Ted Falasco <14131511+tfalasco@users.noreply.github.com>
2025-03-27 06:15:03 -0400

    Add placeholder and test code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
-import "./styles.css";
+import { Todo, Priority } from "./todo-item.js";
+import { Project } from "./project.js";
+import * as storage from "./storage-wrapper.js";
+import { createProjectCard } from "./display-project.js";
+import "./styles/main.css";
 
-// Example image import
-/*
-import imageName from "./images/imageName.svg";
+// createTestData();
 
-const image = document.createElement("img");
-image.src = imageName;
-document.body.appendChild(image);
-*/
+// Display projects
+const projects = fetchAllProjects();
+for (const project of projects) {
+    document.body.appendChild(createProjectCard(project));
+}
+
+function fetchAllProjects() {
+    const projects = new Array();
+    const projectIds = storage.fetchProjectIds();
+    for (const projectId of projectIds) {
+        projects.push(storage.restoreProject(projectId));
+    }
+
+    return projects;
+}
+
+function createTestData() {
+    localStorage.clear();
+    const todoTest1 = new Todo("Test1", "Do this", new Date(), Priority.LOW);
+    todoTest1.title = ("Changed Title");
+    const todoTest2 = new Todo("Test2", "Do that", new Date(), Priority.HIGH);
+    const todoTest3 = new Todo("Test3", "Do the other", new Date(), Priority.MEDIUM);
+    const projectTest1 = new Project("Test PJ1");
+    projectTest1.name = "Changed Test PJ1 name";
+    const projectTest2 = new Project("Test PJ2");
+    projectTest1.addTodo(todoTest1);
+    projectTest1.addTodo(todoTest2);
+    projectTest2.addTodo(todoTest3);
+}
+
